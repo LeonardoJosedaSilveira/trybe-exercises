cd47ba366c8b9d20a5075dd8215175398f66e979
leonardo <rpgleo.js@gmail.com>
2020-11-26 15:21:07 -0300

    4 exercise finished , 5 exercise start

---
+++ fundamentals-of-Web-Development/bloc_4/dia_1/peaceChest.js
@@ -0,0 +1,10 @@
+let rei = 'horizontal vertical ou diagonal uma casa';
+let rainha = 'horizontal vertical ou diagonal, toda linha';
+let torre = 'horizontal vertical, toda linha';
+let bispo = 'diagonal, toda linha';
+let cavalo = 'se move em L 3x2';
+let peão = 'horizontal vertical ou diagonal, so mata na diagonal';
+
+let peca = 'cavalo';
+
+
---
+++ fundamentals-of-Web-Development/bloc_4/dia_1/trueandfalse.js
@@ -0,0 +1,9 @@
+let retaA = 10;
+let retaB = 20;
+let base = 30;
+
+if (retaA + retaB >= base){
+    console.log(retaA + retaB, true);
+} else {
+    console.log(false);
+}
