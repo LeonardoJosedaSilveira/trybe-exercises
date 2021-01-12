38f68bbfb98d624332ab72c4112d8ea22409d7a3
LeonardoJosedaSilveira <rpgleo.js@gmail.com>
2021-01-12 04:01:06 -0300

    exercise 5

--- fundamentals-of-Web-Development/bloc_5/dia_3/script.js
+++ fundamentals-of-Web-Development/bloc_5/dia_3/script.js
@@ -22,7 +22,7 @@ function addDaysOfMonth(){
     const day = document.createElement('li');
     day.innerHTML = dezDaysList[index]
     day.className = 'day';
-    if (dezDaysList[index] === 24 || dezDaysList[index] === 25 || dezDaysList[index] === 30){
+    if (dezDaysList[index] === 24 || dezDaysList[index] === 25 || dezDaysList[index] === 31){
       day.classList.add ('holiday')
     }
     if (dezDaysList[index] === 4 || dezDaysList[index] === 11 || dezDaysList[index] === 18 || dezDaysList[index] === 25){
@@ -35,21 +35,64 @@ function addDaysOfMonth(){
 
 addDaysOfMonth();
 
-// Exercício 2:
-// Implemente uma função que receba como parâmetro a string "Feriados" e crie dinamicamente um botão com o nome "Feriados".
-
-//     Adicione a este botão a ID "btn-holiday" .
-//     Adicione este botão como filho/filha da tag <div> com classe "buttons-container" .
-
-function bntHolidays(Feriados) {
-  const holday = Feriados;
+function bntHF(ButtonName, ButtonID) {
+  const holday = ButtonName;
+  const btid = ButtonID
   const bnt = document.createElement('button');
   let elementFather = document.querySelector('.buttons-container')
 
-  bnt.id = 'btn-holiday';
-  bnt.innerHTML = 'Feriados';
+  bnt.id = ButtonID;
+  bnt.innerHTML = ButtonName;
   elementFather.appendChild(bnt)
   
 }
 
-bntHolidays();
+bntHF('Feriados', 'btn-holiday');
+
+bntHF('Sexta-feira', 'btn-friday')
+
+let clickHolidays = document.getElementById('btn-holiday');
+clickHolidays.addEventListener('click', changeColor);
+
+function changeColor() {
+
+  let dayss = document.querySelectorAll('.holiday')
+  for (let index = 0; index < dayss.length; index++) {
+    let temp = dayss[index].className
+    if (temp === 'day holiday' || temp === 'day holiday friday') {
+      dayss[index].classList.add('changeDay')
+    } else {
+      dayss[index].classList.remove('changeDay')
+    }
+  }
+  
+}
+  
+let clickFriday = document.getElementById('btn-friday');
+clickFriday.addEventListener('click', changeText)
+
+
+
+  
+function changeText() {
+  
+    let textoExibido = 'sextouuu'
+    let dayss = document.querySelectorAll('.friday')
+    const friday = ['4', '11', '18', '25']
+    for (let index = 0; index < dayss.length; index++) {
+      let temp = dayss[index].innerText
+      
+      if (temp === textoExibido) {
+        dayss[index].innerText =friday[index] 
+      } else {
+        dayss[index].innerText = textoExibido
+      }
+    }
+    console.log(textoExibido)
+}
+
+
+// Implemente uma função que adicione ao botão "Sexta-feira" um evento de "click" que modifica o texto exibido nos dias que são Sexta-feira.
+
+//     É interessante que este botão possua também a lógica inversa. Ao ser clicado novamente ele retorna à configuração inicial exibindo os dias.
+
