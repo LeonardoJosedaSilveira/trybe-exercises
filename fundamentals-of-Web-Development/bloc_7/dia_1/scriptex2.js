c38d2a7779b0f62823af43aeb460c31d86b76739
LeonardoJosedaSilveira <rpgleo.js@gmail.com>
2021-01-13 02:20:30 -0300

    second exercise

---
+++ fundamentals-of-Web-Development/bloc_7/dia_1/scriptex2.js
@@ -0,0 +1,7 @@
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+// Seu código aqui.
+ const newOrder = oddsAndEvens.reverse();
+// newOrder.reverse();
+
+console.log(`la vai numero ${newOrder.sort((a, b) => a - b)}`);
